0baee026035ad2f4b43edbc0d7451d0b5fe5d906
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-07-21 14:34:34 +0200

    Initial Commit

---
+++ BookSearch/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BookSearch
+//
+//  Created by Mustafa Ozhan on 21/07/2019.
+//  Copyright © 2019 Mustafa Ozhan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
