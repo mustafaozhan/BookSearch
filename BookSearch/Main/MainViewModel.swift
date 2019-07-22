8156e9b448e97a8b698346ed41165ae3978a0e1f
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2019-07-22 23:11:13 +0200

    ViewModel functionilty is done without pictures

--- BookSearch/Main/MainViewModel.swift
+++ BookSearch/Main/MainViewModel.swift
@@ -21,4 +21,46 @@ final class MainViewModel: BindableObject {
     private (set) var items = [BookDisplayData]() {
         didSet { willChange.send(self) }
     }
+    
+    private (set) var searchCancellable: Cancellable? {
+        didSet {  oldValue?.cancel() }
+    }
+    
+    deinit {
+        searchCancellable?.cancel()
+    }
+    
+    init() {
+        print("Init ViewModel")
+        
+        searchCancellable =  willChange.eraseToAnyPublisher()
+        .map{
+            $0.searchText
+        }
+        .debounce(for: 0.5, scheduler: DispatchQueue.main)
+        .removeDuplicates()
+        .filter({!$0.isEmpty &&  $0.first != " "})
+            .flatMap({(searchString) -> AnyPublisher<[Book], Never> in
+                print("searching: \(searchString)")
+                
+                return APIService.searchBy(title: searchString)
+                .replaceError(with: [])
+                .eraseToAnyPublisher()
+            })
+        .map{
+            self.booksToBookDisplayData(books: $0)
+        }
+        .replaceError(with: [])
+        .assign(to: \.items, on: self)
+    }
+    
+    private func booksToBookDisplayData(books: [Book]) -> [BookDisplayData]  {
+            var displayDataItems = [BookDisplayData]()
+            
+            books.forEach {
+                let displayData = BookDisplayData(id: $0.id, title: $0.volumeInfo?.title ?? "", authors: $0.volumeInfo?.authors ?? [], description: $0.volumeInfo?.description ?? "", thumbnail: $0.volumeInfo?.imageLinks?.thumbnail)
+                displayDataItems.append(displayData)
+            }
+            return displayDataItems
+        }
 }
